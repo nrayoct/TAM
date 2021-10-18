5068c8f9e430e2b7be3baf68443af80b5bf1a702
byebyeblue <engtalker.id@gmail.com>
2021-10-18 17:30:18 +0700

    second commit

--- WeRead/app/src/main/java/com/example/weread/Search.java
+++ WeRead/app/src/main/java/com/example/weread/Search.java
@@ -2,19 +2,31 @@ package com.example.weread;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Search extends AppCompatActivity {
 
+    private EditText mSearchField;
+    private ImageButton mSearchBtn;
+    private RecyclerView mResultList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        //isi search
+        mSearchField =(EditText) findViewById(R.id.search_field);
+        mSearchBtn = (ImageButton) findViewById(R.id.imageButton);
+        mResultList = (RecyclerView) findViewById(R.id.result_list);
 
         //inisialisasi and assign variabel
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
