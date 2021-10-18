44304486d1c7a75ff91d9a5730a303b2d9679de2
nurayu1039 <nurayuoctarina@gmail.com>
2021-10-18 14:06:38 +0700

    first commit

---
+++ WeRead/app/src/main/java/com/example/weread/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.weread;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //inisialisasi and assign variabel
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //set home selected
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        //perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+               switch (menuItem.getItemId()){
+                   case R.id.home:
+//                       startActivity(new Intent(getApplicationContext(), Home.class));
+//                       overridePendingTransition(0,0);
+                       return true;
+                   case R.id.search:
+                       startActivity(new Intent(getApplicationContext(), Search.class));
+                       overridePendingTransition(0,0);
+                       return true;
+                   case R.id.library:
+                       startActivity(new Intent(getApplicationContext(), Library.class));
+                       overridePendingTransition(0,0);
+                       return true;
+               }
+                return false;
+            }
+        });
+
+    }
+}
